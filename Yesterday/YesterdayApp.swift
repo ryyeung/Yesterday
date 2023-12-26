fec0ab592d740ae3cbc2b1066b83fe6ba1e345fc
Ryan Yeung <ryyeung@usc.edu>
2023-12-25 16:50:44 -0800

    Initial Commit

---
+++ Yesterday/YesterdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesterdayApp.swift
+//  Yesterday
+//
+//  Created by Ryan Yeung on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct YesterdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
